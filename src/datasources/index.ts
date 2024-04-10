e52a9a971ca320a822f8b6a7dda8d530487bae92
Jesse Lovelace <jesse.lovelace@gmail.com>
2024-04-10 07:13:57 -0400

    Moving things around.

--- src/datasources/index.ts
+++ src/datasources/index.ts
@@ -1,19 +1,2 @@
-import { GET_AUTHORS } from "./operations";
-import { GetAuthorsQuery } from "./types/graphql";
-import { GraphQLDataSource } from "./GraphQLDataSource";
+export { AuthorDataSource } from "./AuthorDataSource";
 
-export class AuthorDataSource extends GraphQLDataSource {
-
-    public async getAuthors(/* variables */) {
-        return this.execute<GetAuthorsQuery>({
-          query: GET_AUTHORS, variables:{}
-        }).then(({ data, errors }) => {
-          // use error logger
-          if (errors) {
-            throw new Error(errors.map((error) => error.message).join("\n"));
-          }
-          return data;
-        });
-      }
-
-}
